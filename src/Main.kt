cc631f92390d332b2170ce84a4ada7852f04b02a
Antonio Donato <antonio.donato@cgm.com>
2018-12-29 01:13:58 +0100

    rimossa property inutile

--- src/Main.kt
+++ src/Main.kt
@@ -3,8 +3,6 @@ fun main(args: Array<String>) {
     println(variabile)
 }
 
-var varibile: String = ""
-
 fun ScriviCiao(scritta: String) : String{
     println("Hai scritto $scritta")
     return "pippo"
